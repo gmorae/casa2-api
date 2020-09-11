368bbb30701cb3a03d4f7b7a38294cd390c33184
gmorae <gmmartins06@gmail.com>
2020-09-11 19:35:44 -0300

    up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const routes = require('./routes')
 const cors = require('cors')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(routes)
+app.get('/', (_req, res, _next) => {
+  res.status(200).send({
+    title: 'API CASA 2',
+    version: '0.0.1',
+  });
+});
 app.listen(3333 || process.env.PORT)
